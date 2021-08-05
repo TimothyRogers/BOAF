b999f2881fb9a04c37a3e1be5a34a8e43985813a
Tim Rogers <44258477+TimothyRogers@users.noreply.github.com>
2021-08-05 17:14:17 +0100

    Tests import shared fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+import numpy as np
+
+@pytest.fixture(params=[1,2,4])
+def clustering_data(request):
+
+    nclusters = request.param
+    r = 5
+    ang = 2*np.pi/np.arange(1,nclusters+1)[:,None]
+    mean = r*np.hstack((np.cos(ang),np.sin(ang)))
+
+    ndata = 10
+    X = np.random.standard_normal((ndata*nclusters,2)) + np.repeat(mean,ndata, axis=0)
+    # Xt = np.random.standard_normal((ndata*nclusters,2)) + np.repeat(mean,ndata, axis=0)
+    inds = np.repeat(np.arange(nclusters)[:,None],ndata)
+    # inds_t = np.repeat(np.arange(nclusters)[:,None],ndata)
+
+    return nclusters, (X, inds),
--- tests/test_kmeans.py
+++ tests/test_kmeans.py
@@ -5,22 +5,7 @@ np.random.seed(1)
 
 from boaf.kmeans import KMeans
 
-@pytest.fixture(params=[1,2,4])
-def clustering_data(request):
-
-    nclusters = request.param
-    r = 5
-    ang = 2*np.pi/np.arange(1,nclusters+1)[:,None]
-    mean = r*np.hstack((np.cos(ang),np.sin(ang)))
-
-    ndata = 10
-    X = np.random.standard_normal((ndata*nclusters,2)) + np.repeat(mean,ndata, axis=0)
-    # Xt = np.random.standard_normal((ndata*nclusters,2)) + np.repeat(mean,ndata, axis=0)
-    inds = np.repeat(np.arange(nclusters)[:,None],ndata)
-    # inds_t = np.repeat(np.arange(nclusters)[:,None],ndata)
-
-    return nclusters, (X, inds),
-
+@pytest.mark.usefixtures("clustering_data")
 def test_kmeans(clustering_data):
     nclusters, (X, inds) = clustering_data
 
